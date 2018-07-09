c71ab1b749505174f0d865cd2790a2e263b70f49
Galuh Estya <galuhestya@gmail.com>
2018-07-09 17:42:52 +0700

    benerin spacing

--- crud_test.go
+++ crud_test.go
@@ -5,89 +5,89 @@ import "gopkg.in/DATA-DOG/go-sqlmock.v1"
 func TestGetUser(t *testing.T)  {
     rows := sqlmock.NewRows([]string{"name"}).
     AddRow("item N")
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-	mock.ExpectPrepare("SELECT (.*) FROM (.*)").ExpectQuery().WithArgs(1).WillReturnRows(rows)
+    db, mock, err := sqlmock.New()
+    if err != nil {
+        t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+    }
+    defer db.Close()
+    mock.ExpectPrepare("SELECT (.*) FROM (.*)").ExpectQuery().WithArgs(1).WillReturnRows(rows)
     res, err := GetItem(db,1)
     if err != nil {
         t.Errorf("something goes wrong 500")
     }
-	if res != "{\"status\":\"OK\",\"payload\":{\"name\":\"item N\"}}" {
-		t.Errorf("wrong json")
-	}
-	// we make sure that all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
+    if res != "{\"status\":\"OK\",\"payload\":{\"name\":\"item N\"}}" {
+        t.Errorf("wrong json")
+    }
+    // we make sure that all expectations were met
+    if err := mock.ExpectationsWereMet(); err != nil {
+        t.Errorf("there were unfulfilled expectations: %s", err)
+    }
 }
 
-func TestCreateUser(t *testing.T)	{
+func TestCreateUser(t *testing.T)   {
     var err error
-	rows := sqlmock.NewRows([]string{"id"}).
+    rows := sqlmock.NewRows([]string{"id"}).
     AddRow("1")
 
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-	mock.ExpectPrepare("INSERT (.*)").ExpectExec().WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectPrepare("SELECT (.*)").ExpectQuery().WillReturnRows(rows)
-	// fmt.Println(CreateItem(db,"item N"))
+    db, mock, err := sqlmock.New()
+    if err != nil {
+        t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+    }
+    defer db.Close()
+    mock.ExpectPrepare("INSERT (.*)").ExpectExec().WillReturnResult(sqlmock.NewResult(1, 1))
+    mock.ExpectPrepare("SELECT (.*)").ExpectQuery().WillReturnRows(rows)
+    // fmt.Println(CreateItem(db,"item N"))
     res, err := CreateItem(db,"item N")
     if err != nil {
         t.Errorf("something goes wrong 500")
     }
-	if res != "{\"status\":\"OK\",\"payload\":{\"id\":\"1\"}}" {
-		t.Errorf("wrong json")
-	}
-	// we make sure that all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
+    if res != "{\"status\":\"OK\",\"payload\":{\"id\":\"1\"}}" {
+        t.Errorf("wrong json")
+    }
+    // we make sure that all expectations were met
+    if err := mock.ExpectationsWereMet(); err != nil {
+        t.Errorf("there were unfulfilled expectations: %s", err)
+    }
 }
 
-func TestUpdateUser(t *testing.T)	{
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-	mock.ExpectPrepare("update (.*)").ExpectExec().WithArgs("item N new!", 1).WillReturnResult(sqlmock.NewResult(1, 1))
-	// fmt.Println(CreateItem(db,"item N"))
+func TestUpdateUser(t *testing.T)   {
+    db, mock, err := sqlmock.New()
+    if err != nil {
+        t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+    }
+    defer db.Close()
+    mock.ExpectPrepare("update (.*)").ExpectExec().WithArgs("item N new!", 1).WillReturnResult(sqlmock.NewResult(1, 1))
+    // fmt.Println(CreateItem(db,"item N"))
     res, err := UpdateItem(db,1, "item N new!")
-	if err != nil  {
+    if err != nil  {
         t.Errorf("something goes wrong 500")
     }
     if res != "{\"status\":\"OK\"}" {
-		t.Errorf("wrong json")
-	}
-	// we make sure that all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
+        t.Errorf("wrong json")
+    }
+    // we make sure that all expectations were met
+    if err := mock.ExpectationsWereMet(); err != nil {
+        t.Errorf("there were unfulfilled expectations: %s", err)
+    }
 }
 
-func TestDeleteUser(t *testing.T)	{
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-	mock.ExpectPrepare("delete (.*)").ExpectExec().WithArgs(1).WillReturnResult(sqlmock.NewResult(1, 1))
-	// fmt.Println(CreateItem(db,"item N"))
+func TestDeleteUser(t *testing.T)   {
+    db, mock, err := sqlmock.New()
+    if err != nil {
+        t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+    }
+    defer db.Close()
+    mock.ExpectPrepare("delete (.*)").ExpectExec().WithArgs(1).WillReturnResult(sqlmock.NewResult(1, 1))
+    // fmt.Println(CreateItem(db,"item N"))
     res, err := DeleteItem(db,1)
-	if err != nil {
+    if err != nil {
         t.Errorf("something goes wrong")
     }
     if res != "{\"status\":\"OK\"}" {
-		t.Errorf("wrong json")
-	}
-	// we make sure that all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
+        t.Errorf("wrong json")
+    }
+    // we make sure that all expectations were met
+    if err := mock.ExpectationsWereMet(); err != nil {
+        t.Errorf("there were unfulfilled expectations: %s", err)
+    }
 } 
